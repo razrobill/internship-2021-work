74feff4111b4eacaa8cde734e2fd3b245e82fdd2
razrobill <tb1126@york.ac.uk>
2021-09-24 16:29:12 +0100

    updates

---
+++ debugging stuff/making modules/mymodule.py
@@ -0,0 +1,10 @@
+def welcome(name):
+    print("Welcome, " + name +" to my website")
+
+person1 = {
+    "name": "Talia",
+    "age": "21",
+    "country": "England",
+    "education": "University of York"
+
+}
---
+++ debugging stuff/making modules/using-my-module.py
@@ -0,0 +1,12 @@
+import mymodule
+mymodule.welcome("Talia Blain")
+
+
+
+import mymodule as new_module
+a = new_module.person1["age"]
+b = new_module.person1["education"]
+c = new_module.person1["country"]
+print(a)
+print(b)
+print(c)
